dbff8e9d87eb479496694ea9c20cd41e08776fa9
twct <timothy@thrift.dev>
2022-11-17 20:22:11 -0600

    Change counting mechanism for duplicate nodes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use crate::error::Error;
 mod env;
 mod error;
 
+type NodeCountHashMap = HashMap<String, u64>;
+
 #[derive(Deserialize, Debug)]
 struct Node {
     id: String,
@@ -23,22 +25,24 @@ async fn call_api<T: DeserializeOwned>(node_ids: &str) -> Result<T, Error> {
 }
 
 #[async_recursion]
-async fn visit(node: &Node, hash_map: &mut HashMap<String, Vec<String>>) -> Result<(), Error> {
-    match hash_map.get_mut(&node.id) {
-        Some(vec) => vec.push(node.id.to_owned()),
+async fn visit(node: &Node, node_count_hash_map: &mut NodeCountHashMap) -> Result<(), Error> {
+    println!("Visiting node {:?}", node.id);
+
+    match node_count_hash_map.get_mut(&node.id) {
+        Some(count) => {
+            *count = *count + 1
+        },
         None => {
-            hash_map.insert(node.id.to_owned(), vec![node.id.to_owned()]);
+            node_count_hash_map.insert(node.id.to_owned(), 1);
         }
     }
 
-    let child_node_ids = &node.child_node_ids.join(",");
-
-    if !child_node_ids.is_empty() {
+    if node.child_node_ids.len() > 0 {
+        let child_node_ids = &node.child_node_ids.join(",");
         let nodes = call_api::<Vec<Node>>(&child_node_ids).await?;
 
         for node in nodes {
-            println!("Visiting node {:?}", node.id);
-            visit(&node, hash_map).await?;
+            visit(&node, node_count_hash_map).await?;
         }
     }
 
@@ -51,22 +55,21 @@ async fn main() -> Result<(), Error> {
 
     let root_node_id = "089ef556-dfff-4ff2-9733-654645be56fe";
     
-    let mut hash_map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut node_count_hash_map: NodeCountHashMap = HashMap::new();
 
     let nodes = call_api::<Vec<Node>>(root_node_id).await?;
 
     for node in nodes {
-        visit(&node, &mut hash_map).await?;
+        visit(&node, &mut node_count_hash_map).await?;
     }
 
-    println!("Unique IDs: {}", hash_map.keys().len());
-
-    match hash_map.iter().max_by_key(|(_k, v)| v.len()).map(|(k, v)| (k, v)) {
-        Some((id, vec)) => {
-            println!("Most common: {:?}, {} time(s)", id, vec.len());
+    match node_count_hash_map.iter().max_by(|a, b| a.1.cmp(&b.1)) {
+        Some((most_common_id, most_common_count)) => {
+            println!("Unique IDs: {}", node_count_hash_map.keys().len());
+            println!("Most common: {:?}, {} time(s)", most_common_id, most_common_count);
         },
         None => {
-            eprintln!("Failed to find most common node ID");
+            println!("No nodes found");
         }
     }
 
